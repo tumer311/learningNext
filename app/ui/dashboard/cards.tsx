b7f84a3ca47c05fc53b0bdc6581b982e4fa18a09
Bekir <36570425+tumer311@users.noreply.github.com>
2024-06-14 20:55:52 +0300

    Trello Github deneme

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -54,6 +54,7 @@ export function Card({
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        <h3 className="ml-2 text-sm font-medium">Github Power up added</h3>
       </div>
       <p
         className={`${lusitana.className}
